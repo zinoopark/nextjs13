6bc2fe43eb5eb9878c7f506e17113a21b157b23f
JH Lee <13332150+oulico@users.noreply.github.com>
2023-06-21 20:59:55 +0900

    FEAT: KAKAO OAuth 추가

--- src/app/api/auth/[...nextauth]/route.tsx
+++ src/app/api/auth/[...nextauth]/route.tsx
@@ -1,12 +1,24 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
+// import AppleProvider from "next-auth/providers/apple";
+import KakaoProvider from "next-auth/providers/kakao";
 
 const handler = NextAuth({
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID ?? "",
       clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? ""
+    }),
+    // AppleProvider({
+    //   clientId: process.env.APPLE_ID ?? "",
+    //   clientSecret: process.env.APPLE_SECRET ?? ""
+    // }),
+    KakaoProvider({
+      clientId: process.env.KAKAO_CLIENT_ID ?? "",
+      clientSecret: process.env.KAKAO_CLIENT_SECRET ?? ""
     })
+
+
   ]
 });
 
