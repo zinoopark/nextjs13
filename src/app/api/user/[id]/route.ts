78464f8cbf7f754941dd6a70654096f7dd769aa2
JH Lee <13332150+oulico@users.noreply.github.com>
2023-07-19 21:21:22 +0900

    FEAT: added jwt token

---
+++ src/app/api/user/[id]/route.ts
@@ -0,0 +1,19 @@
+import prisma from "@/lib/prisma";
+import { verifyJwt } from "@/lib/jwt";
+
+export async function GET(request: Request, { params }: { params: { id: number } }) {
+  const accessToken = request.headers.get("authorization");
+  if (!accessToken || !verifyJwt(accessToken)) return new Response(JSON.stringify({ error: "unauthorized" }), { status: 401 });
+  const userPosts = await prisma.post.findMany({
+    where: { authorId: +params.id },
+    include: {
+      author: {
+        select: {
+          email: true,
+          name: true
+        }
+      }
+    }
+  });
+  return new Response(JSON.stringify(userPosts));
+}
